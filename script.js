51478157884abaf8593008c52cce036c9bc90d47
HariMathivanan <haritka999@gmail.com>
2025-06-03 15:38:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ function calculateBill() {
   // Implement your tariff rates here (e.g., tiered pricing)
 
   if (units > 1000){
-    bill = units*11.80;
+    bill = (units-100)*11.80;
   } else if (units > 800 && units <= 1000){
-    bill = units*10.7;
+    bill = (units-100)*10.7;
   } else if (units > 600 && units <= 800){
-    bill = units*9.65;
+    bill = (units-100)*9.65;
   } else if (units > 500 && units <= 600){
-    bill = units*8.55;
+    bill = (units-100)*8.55;
   } else if (units > 400 && units <= 500){
-    bill = units*6.45;
+    bill = (units-100)*6.45;
   } else if (units > 101 && units <= 400){
-    bill = units*4.8;
+    bill = (units-100)*4.8;
   } else if (units <= 100) {
     bill = 0;
   }
