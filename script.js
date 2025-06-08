b8e9713f2de7dd17c091061ef19e7f23b69b3728
HariMathivanan <haritka999@gmail.com>
2025-06-08 13:10:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ function calculateBill() {
 
   // Implement your tariff rates here (e.g., tiered pricing)
 
-  if (units > 1000){
+if ((units-100) > 1000){
     bill = (units-100)*11.80;
-  } else if (units > 800 && units <= 1000){
+  } else if ((units-100) > 800 && (units-100) <= 1000){
     bill = (units-100)*10.7;
-  } else if (units > 600 && units <= 800){
+  } else if ((units-100) > 600 && (units-100) <= 800){
     bill = (units-100)*9.65;
-  } else if (units > 500 && units <= 600){
+  } else if ((units-100) > 500 && (units-100) <= 600){
     bill = (units-100)*8.55;
-  } else if (units > 400 && units <= 500){
+  } else if ((units-100) > 400 && (units-100) <= 500){
     bill = (units-100)*6.45;
-  } else if (units > 101 && units <= 400){
+  } else if ((units-100) > 100 && (units-100) <= 400){
     bill = (units-100)*4.8;
-  } else if (units <= 100) {
-    bill = 0;
+  } else if ((units-100) >= 1) {
+    bill = (units-100)*4.8;
   }
   document.getElementById("result").textContent = "Your Bill: ₹" + bill;
 }
